629336e8c64cc45b5aac122beee7d43f9395a547
Nick McSpadden <nmcspadden@gmail.com>
2014-09-08 13:11:52 -0700

    Fully working

--- jsspull.py
+++ jsspull.py
@@ -30,19 +30,19 @@ def getDeviceListFromJSS():
 	print "stuff"
 
 
-mysql_host = "mysql"
+mysql_host = "host"
 mysql_user = "user"
 mysql_password = "password"
 mysql_db = "db"
 
 ## Update the data in the SQL table
-def SubmitSQLForDevice(thisDevice, conn):
-		device_name = thisDevice['mobile_device']['general']['name']
-		device_username = thisDevice['mobile_device']['location']['username']
+def SubmitSQLForDevice(thisDevice, conn, j):
+		device_name = thisDevice['device_name']
+		device_username = thisDevice['username']
 		
 		sql = """INSERT INTO casperimport (id, serial, name, model, ios_version, ipaddress, macaddress, bluetooth, capacity, username, email, asset_tag) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE serial=VALUES(serial), name=VALUES(name), model=VALUES(model), ios_version=VALUES(ios_version), ipaddress=VALUES(ipaddress), macaddress=VALUES(macaddress), bluetooth=VALUES(bluetooth), capacity=VALUES(capacity), username=VALUES(username), email=VALUES(email), asset_tag=VALUES(asset_tag)"""
 		
-		sql_values=[ thisDevice['mobile_device']['general']['id'], thisDevice['mobile_device']['general']['serial_number'], (device_name or u'').encode('unicode_escape').decode().encode('latin1'), thisDevice['mobile_device']['general']['modelDisplay'], thisDevice['mobile_device']['general']['os_version'], thisDevice['mobile_device']['general']['ip_address'], thisDevice['mobile_device']['general']['wifi_mac_address'], thisDevice['mobile_device']['general']['bluetooth_mac_address'], GetHumanReadable(int(thisDevice['mobile_device']['general']['capacity'])), (device_username or u'').encode('unicode_escape').decode().encode('latin1'), thisDevice['mobile_device']['location']['email_address'], thisDevice['mobile_device']['general']['asset_tag'] ]
+		sql_values=[ thisDevice['id'], thisDevice['serial_number'], (device_name or u'').encode('unicode_escape').decode().encode('latin1'), thisDevice['modelDisplay'], j.MobileDevice(thisDevice['id']).findtext('general/os_version'), j.MobileDevice(thisDevice['id']).findtext('general/ip_address'), thisDevice['wifi_mac_address'], j.MobileDevice(thisDevice['id']).findtext('general/bluetooth_mac_address'), GetHumanReadable(int(j.MobileDevice(thisDevice['id']).findtext('general/capacity'))), (device_username or u'').encode('unicode_escape').decode().encode('latin1'), j.MobileDevice(thisDevice['id']).findtext('location/email_address'), j.MobileDevice(thisDevice['id']).findtext('location/asset_tag') ]
 		
 		cursor = conn.cursor()
 		cursor.execute(sql, sql_values)
@@ -57,7 +57,7 @@ def main():
 		sys.exit(1)
 	
 	try:
-		jss_prefs = jss.JSSPrefs()
+		jss_prefs = jss.JSSPrefs("com.github.sheagcraig.python-jss.plist")
 		j = jss.JSS(jss_prefs)
 	except:
 		print "Couldn't access preferences file."
@@ -66,7 +66,7 @@ def main():
 	deviceList = j.MobileDevice()
 
 	for device in deviceList:
-		SubmitSQLForDevice(device, conn)
+		SubmitSQLForDevice(device, conn, j)
 	
 	conn.commit()
 	
